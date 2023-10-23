5bff7c89bbefeaa7cc0f1be2c7c97a6479e9d00b
nourg2025 <ngabsi@ucsc.edu>
2023-10-23 09:03:21 -0700

    change

---
+++ lab4/js/lab4.js
@@ -0,0 +1,23 @@
+// Rock_Paper_Scissors
+// An implementation of the game
+// @author Nour Gabsi and Bryon James 
+// October 23rd 2023
+
+// Ask for rock, paper, or scissors
+// Collect user input
+// Randomly select rock, paper, or scissors for computer input
+// Calculate results
+// Tell user if they won, lost, or tied
+// Let the player start again
+
+//Baking Cookies
+//Gather necessary ingredients and tools
+//Preheat oven
+//Beat butter, white sugar, and brown sugar in a large bowl
+//Beat in eggs and stir in vanilla
+//Dissolve baking soda in hot water
+//Add dissolved baking soda and salt to batter
+//Stir in flour and chocolate chips
+//Add spoonfuls of batter to baking sheet
+//Bake for about 10 minutes
+//Let cookies cool briefly before eating
