c0d0e0a6509efd1902a6f1679b000567fe29369b
nourg2025 <ngabsi@ucsc.edu>
2023-10-25 20:29:00 -0700

    change js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+* Author: Nour Gabsi <ngabsi@ucsc.edu>
+* Created: 25 October
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+// calculate
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br");
+document.writeln("Age: " + age + " years<br>");
