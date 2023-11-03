b5b4af4944cbb1e3a5660d19b5555aa1284e2f8b
nourg2025 <ngabsi@ucsc.edu>
2023-11-02 17:24:55 -0700

    change

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -1,22 +1,33 @@
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
+// index.js - purpose and description here
+// Author: Nour Gabsi <ngabsi@ucsc.edu>
+// Date: November 3 2023
+
+// Constants
+
+// Functions
 function sortUserName() {
-    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-    console.log("userName =", userName);
+    var userName = window.prompt("Hello, what name needs repairing?");
+    console.log("userName = ", userName);
     // split string to array
     var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
+    console.log("nameArray = ", nameArray);
     // sort the array
     var nameArraySort = nameArray.sort();
-    console.log("nameArraySort =", nameArraySort);
+    console.log("nameArraySort = ", nameArraySort);
     // join array back to a string
     var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-    // Note that I could have done the above lines as a single line:
-    // userName.toLower().|split("").sort().join("")
+    console.log("nameSorted = ", nameSorted);
     return nameSorted;
 }
 
-// output
-document.writeln("Oh hey, I've fixed your name:",
+function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+}
+
+// let's get this party started
+main();
+
+// Output
+document.writeln("I've fixed your name: ",
 sortUserName(), "</br>");
