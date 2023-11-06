7eadb4c476bdcefc091085b2aeee348f8e0e2f77
nourg2025 <ngabsi@ucsc.edu>
2023-11-06 08:19:51 -0800

    remove

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    // start by finding element
-        outputEl = document.getElementById("output");
-    // change text in element
-        outputEl.innerHTML = "Yo, 'sup?";
-    }
-    
