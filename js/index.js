ed23b924edd85159fd0b9eae4b32b8e10f5fb249
nourg2025 <ngabsi@ucsc.edu>
2023-11-06 08:19:12 -0800

    ert

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function main() {
+    // start by finding element
+        outputEl = document.getElementById("output");
+    // change text in element
+        outputEl.innerHTML = "Yo, 'sup?";
+    }
+    
