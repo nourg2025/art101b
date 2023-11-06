f9579e37c728f31066f4ef747f71931b7a13a505
nourg2025 <ngabsi@ucsc.edu>
2023-11-05 23:47:30 -0800

    chabge

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,28 +1,91 @@
-/*
-* Author: Nour Gabsi <ngabsi@ucsc.edu>
-* Created: 4 November
-* License: Public Domain
-*/
+// Lab 8 - Anon Functions and Callbacks
+// Nour Gabsi <ngabsi@ucsc.edu>
+// 11/6/2023
 
-function isEven(x){
-    return (x % 2 == 0);
+// variables
+var arr = [ 3, 10, 65, 8 ];
+
+
+// named functions
+function nearestEven(x) {
+    if (x % 2 === 1) {
+        return (x - 1);
+    }
+    else {
+        return x;
+    }
 }
 
-// test function
-console.log("Is 1 even? ", isEven(1));
-console.log("Is 2 even? ", isEven(2));
 
-array = [100, 81, 4, 16, 42, 144, 10000]
-console.log("My array", array);
+// main
 
-var result = array.map(isEven);
-// should return [true, false, true, true, true, true, true]
-console.log("Test of evenness of array:", result);
+console.log("Nearest even of 3:", nearestEven(3));
+console.log("Nearest even of 10:", nearestEven(10));
 
-var result = array.map(function(x){
-    return x ** 0.5;
+var neResult = arr.map(nearestEven);
+var twoDownResult = arr.map(function(x) {
+    return (x-2);
 })
-// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
-console.log("Squareroot of array:", result);
+
+console.log("Original array: [" + arr +"]");
+console.log(`Nearest even'd array: [${neResult}]` );
+console.log(`Array taken down a couple pegs: [${twoDownResult}]`);
+
+
+// Task X 1 - output on page using .getElementById() and .innerHTML()
 var outputEl = document.getElementById("output"); 
+var mapResults = "Original array: [" + arr +"]<br>"+`Nearest even'd array: [${neResult}]<br>`+`Array taken down a couple pegs: [${twoDownResult}]<br>`
 outputEl.innerHTML = mapResults; // put your results here
+
+// Task X 2 - common methods that use callbacks! .sort(), setTimeout()
+//.sort()
+function reverseSort(a,b) {
+    return b-a;
+}
+console.log(`Reverse sorted array: [${arr.sort(reverseSort)}]`);
+var sortResult = mapResults + `Reverse sorted array: [${arr.sort(reverseSort)}]<br>`;
+outputEl.innerHTML = sortResult;
+
+// setTimeout()
+function surprise() {
+    temp = outputEl.innerHTML;
+    console.log("Surprise!")
+    outputEl.innerHTML = temp + "<div id=\"surprise\">Surprise!<div>";
+}
+setTimeout(surprise, 5000);
+
+// Task X 3
+// decorative
+function padding(x, len) {
+    var length = (len - x.length);
+    var padded = ' ੈ✩‧₊˚';
+    var i = 0;
+    while (i < ((Math.floor(length/2)*2)-5)) {
+        padded += " ";
+        ++i;
+    }
+    padded += x;
+    i = 0;
+    while (i < ((Math.ceil(length/2)*2))) {
+        padded += " ";
+        ++i;
+    }
+    padded += ' ੈ✩‧₊˚';
+    return padded;
+}
+
+function printStars(callbackFn) {
+    var compliments = ["I love you!", "Have a great day!", "You're lovely!", "You look great!", 
+                "Good work today!", "Drink some water!", "You're wonderful!"]
+    var words = callbackFn(compliments[Math.floor(Math.random() * (compliments.length))], 33);
+    var final = "☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆<br>"+
+                ' ੈ✩‧₊˚                                                          ੈ✩‧₊˚<br>'+
+                    words+
+                '<br> ੈ✩‧₊˚                                                          ੈ✩‧₊˚<br>'+
+                "☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆"
+    console.log("Stars printed!");
+    document.getElementById("stars").innerHTML = final;
+    return;
+}
+
+printStars(padding);
