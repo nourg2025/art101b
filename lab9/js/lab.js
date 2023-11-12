623b27daaf44832ef5f282535af44f4b4e7a71d1
nourg2025 <ngabsi@ucsc.edu>
2023-11-12 15:04:40 -0800

    change

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,17 +3,17 @@
 // 11/13/23
 
 // make buttons in each sections
-$(".minor-section").append("<button class='special-button'>Make Special!!!!!!</button>");
+$(".minor-section").append("<button class='special-button'>Make Special</button>");
 
 // make each button toggle "special" for the section it is in on
 $(".special-button").click(function(){ 
-    $(this).parent().toggleClass("special!!!!!!");
+    $(this).parent().toggleClass("special");
     // text toggle (extra)
-    if ($(this).parent().hasClass("special!!!!!!")) {
-        $(this).html("Make Unspecial:(");
+    if ($(this).parent().hasClass("special")) {
+        $(this).html("Make Unspecial");
     }
     else {
-        $(this).html("Make Special!!!!!!");
+        $(this).html("Make Special");
     }
 })
 
