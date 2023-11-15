4301c87a66c8b9106831816e4d0ecaf47773efd8
nourg2025 <ngabsi@ucsc.edu>
2023-11-15 12:17:18 -0800

    change

---
+++ lab10/js/lab.js
@@ -0,0 +1,28 @@
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Nour Gabsi
+   Date: November 17, 2023
+*/
+
+// Helper function to generate random text
+function generateRandomText() {
+    const text = "Florum blipsum dolor nit amet, confetti adipiscing doodle. Wobble do hiccup tempor splish-splash ut giggle et doodle magna waffle. Ut yippee ad minim boop, quis noodle exercitation jibberish laboris doodle ut aliquip ex ea fancy-pants consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+  
+  // Event listener for button click
+  $("#make-convo").click(function() {
+    // Get new fake dialogue
+    const newText = generateRandomText();
+    // Append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  });
