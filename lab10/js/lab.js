eaca20fdc375032ca71bc4d4cedd4c924d84aa4c
nourg2025 <ngabsi@ucsc.edu>
2023-11-15 12:22:06 -0800

    change

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -9,7 +9,7 @@
 
 // Helper function to generate random text
 function generateRandomText() {
-    const text = "Florum blipsum dolor nit amet, confetti adipiscing doodle. Wobble do hiccup tempor splish-splash ut giggle et doodle magna waffle. Ut yippee ad minim boop, quis noodle exercitation jibberish laboris doodle ut aliquip ex ea fancy-pants consequat.";
+    const text = "Lorem blipsum sit doodle, consectetur flibberish noodle. Sed do hiccup tempor wobble ut giggle et doodle magna waffle. Ut yippee ad minim boop, quis noodle exercitation jibberish laboris doodle ut aliquip ex ea fancy-pants consequat.";
     const min = 3;
     const max = 100;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -21,8 +21,12 @@ function generateRandomText() {
   
   // Event listener for button click
   $("#make-convo").click(function() {
-    // Get new fake dialogue
-    const newText = generateRandomText();
+    // Get user input
+    const userInput = $("#userInput").val();
+    
+    // If user input is provided, use it; otherwise, generate random text
+    const newText = userInput.trim() !== '' ? userInput : generateRandomText();
+    
     // Append a new div to our output div
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
   });
