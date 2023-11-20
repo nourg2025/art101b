1bdee1076d45835c8ad32efd6ab881a77a31ee64
nourg2025 <ngabsi@ucsc.edu>
2023-11-20 10:48:59 -0800

    change

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -2,26 +2,28 @@
 //* Created: November 20, 2023
 //* License: Public Domain
 //*
-
-// Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
-// depending on length mod 4
-function sortingHat(str) {
-    var len = str.length; // or use let or const
-    var mod = len % 4;    // or use let or const
-    if (mod == 0) {
-        return "Gryffindor";
-    } else if (mod == 1) {
-        return "Ravenclaw";
-    } else if (mod == 2) {
-        return "Slytherin";
-    } else if (mod == 3) {
-        return "Hufflepuff";
+document.addEventListener("DOMContentLoaded", function() {
+    // Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+    // depending on length mod 4
+    function sortingHat(str) {
+        var len = str.length;
+        var mod = len % 4;
+        if (mod == 0) {
+            return "Gryffindor";
+        } else if (mod == 1) {
+            return "Ravenclaw";
+        } else if (mod == 2) {
+            return "Slytherin";
+        } else if (mod == 3) {
+            return "Hufflepuff";
+        }
     }
-}
-var myButton = document.getElementById("button");
-myButton.addEventListener("click", function() {
-    var name = document.getElementById("input").value;
-    var house = sortingHat(name);
-    newText = "<p>The Sorting Hat has sorted you into" + house + "</p>";
-    document.getElementById("output").innerHTML = newText;
-})
+
+    var myButton = document.getElementById("button");
+    myButton.addEventListener("click", function() {
+        var name = document.getElementById("input").value;
+        var house = sortingHat(name);
+        newText = "<p>The Sorting Hat has sorted you into " + house + ".</p>";
+        document.getElementById("output").innerHTML = newText;
+    });
+});
