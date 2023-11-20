8563ee0124c440328c5c2942a738fbd8b3b7b68f
nourg2025 <ngabsi@ucsc.edu>
2023-11-20 10:42:42 -0800

    change

---
+++ lab12/js/lab12.js
@@ -0,0 +1,31 @@
+//* Author: Nour Gabsi <ngabsi@ucsc.edu>
+//* Created: November 20, 2023
+//* License: Public Domain
+//*
+
+// Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+// depending on length mod 4
+function sortingHat(str) {
+    len = str.length;
+    mod = len % 4;
+    if (mod == 0) {
+        return "Gryffindor"
+    }
+    else if (mod == 1) {
+        return "Ravenclaw"
+    }
+    else if (mod == 2) {
+        return "Slytherin"
+    }
+    else if (mod == 3) {
+        return "Hufflepuff"
+    }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p>The Sorting Hat has sorted you into" + house + "</p>";
+    document.getElementById("output").innerHTML = newText;
+})
