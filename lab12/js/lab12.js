873f6fffddb2cb78b300e5fbb248603e60d6d942
nourg2025 <ngabsi@ucsc.edu>
2023-11-20 10:46:23 -0800

    change

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -6,22 +6,18 @@
 // Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
 // depending on length mod 4
 function sortingHat(str) {
-    len = str.length;
-    mod = len % 4;
+    var len = str.length; // or use let or const
+    var mod = len % 4;    // or use let or const
     if (mod == 0) {
-        return "Gryffindor"
-    }
-    else if (mod == 1) {
-        return "Ravenclaw"
-    }
-    else if (mod == 2) {
-        return "Slytherin"
-    }
-    else if (mod == 3) {
-        return "Hufflepuff"
+        return "Gryffindor";
+    } else if (mod == 1) {
+        return "Ravenclaw";
+    } else if (mod == 2) {
+        return "Slytherin";
+    } else if (mod == 3) {
+        return "Hufflepuff";
     }
 }
-
 var myButton = document.getElementById("button");
 myButton.addEventListener("click", function() {
     var name = document.getElementById("input").value;
