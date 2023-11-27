68c79ddd0789f2ec914e79977aca4d836506d28d
nourg2025 <ngabsi@ucsc.edu>
2023-11-27 08:44:01 -0800

    change

--- experiments for games kids/js/lab.js
+++
@@ -1,9 +0,0 @@
-// lab.js
-
-$(document).ready(function () {
-    // Function to handle a click event on the game container
-    $('#game-container').on('click', function () {
-        // Change the text inside the game container
-        $(this).html('<p>You clicked! Game in progress...</p>');
-    });
-});
