05310a3bf8ce385a4ae6f39034b94f404a301586
nourg2025 <ngabsi@ucsc.edu>
2023-12-07 20:54:00 -0800

    change

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,30 +1,52 @@
-// Author: Nour Gabsi
-// Created: December 7, 2023
+// Author: Megan Bustamante
+// Created December 4, 2023
+// 
 
 const ENDPOINT = "https://xkcd.com/info.0.json";
 
-$("#button").click(function () {
-    const comicObj = {
-        url: ENDPOINT,
+$("#button").click(function(){
+     
+    const comicObj ={
+        url:ENDPOINT,
         type: "GET",
         dataType: "json",
         success: comicSuccess,
         error: comicError
-    };
 
-    $.ajax(comicObj);
-});
+    }
+    $.ajax(comicObj)
 
-function comicSuccess(comicObj) {
-    console.log("Here is everything", comicObj);
+function comicSuccess(comicObj){
+    console.log("Here's everything", comicObj);
     const objTitle = comicObj.title;
-    const objImg = comicObj.img;
+    const objImg =comicObj.img;
     const objAlt = comicObj.alt;
 
-    $("#output").append("<h2>" + objTitle + "</h2>");
-    $("#output").append("<img src='" + objImg + "' alt='" + objAlt + "' />");
-    $("#output").append("<p>" + objAlt + "</p>");
+$("#output").append("<h2>" + objTitle)
+$("#output").append("<img src='" + objImg + "' />" )
+$("#output").append("<p>" + objAlt)
 }
 
-function comicError(data) {
+function comicError(data){
+    
 }
+
+// // ajax function
+// 
+})
+
+// $.ajax({
+//     url: ENDPOINT,
+//     type: "GET",
+//     dataType : "json",
+
+//     // what the api should do when successful
+//     success: function(data){
+//     // have it connect to xKCD
+//     console.log(data)
+
+//     },
+//     error: function(jqXHR, textStatus, errorThrown){
+//         console.log("Error:", textStatus, errorThrown);
+//     }
+// })
